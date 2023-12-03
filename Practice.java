858af71481b1f0ae109d07840c4697fe4717556c
Balakarthikeyan <balakarthikeyan.d@zohocorp.com>
2023-12-03 14:04:41 +0530

    Practice update

--- Practice.java
+++ Practice.java
@@ -1,9 +1,60 @@
 public class Practice {
     public static void main(String[] args) {
-        int a=000000000000000000000000100111111;
-        int ham=0;
-        for (int i=31;i>=0;i--){
-            System.out.println((a>>i)&1);
+        int[][] points={{1,1},{3,4},{-1,0}};
+        int out=0;
+        for(int i=0;i+1<points.length;i++){
+            int[] a=points[i];
+            int[] b=points[i+1];
+            while(true){
+                if(a[0]>b[0] && a[1]>b[1]){
+                    a[0]=a[0]-1;
+                    a[1]=a[1]-1;
+                    out++;
+                    continue;
+                }
+                if(a[0]<b[0] && a[1]<b[1]){
+                    a[0]=a[0]+1;
+                    a[1]=a[1]+1;
+                    out++;
+                    continue;
+                }
+                if(a[0]>b[0] && a[1]<b[1]){
+                    a[0]=a[0]-1;
+                    a[1]=a[1]+1;
+                    out++;
+                    continue;
+                }
+                if(a[0]<b[0] && a[1]>b[1]){
+                    a[0]=a[0]+1;
+                    a[1]=a[1]-1;
+                    out++;
+                    continue;
+                }
+                if(a[0]==b[0] && a[1]>b[1]){
+                    a[1]=a[1]-1;
+                    out++;
+                    continue;
+                }
+                if(a[0]>b[0] && a[1]==b[1]){
+                    a[0]=a[0]-1;
+                    out++;
+                    continue;
+                }
+                if(a[0]==b[0] && a[1]<b[1]){
+                    a[1]=a[1]+1;
+                    out++;
+                    continue;
+                }
+                if(a[0]<b[0] && a[1]==b[1]){
+                    a[0]=a[0]+1;
+                    out++;
+                    continue;
+                }
+                if(a[0]==b[0] && a[1]==b[1]){
+                    break;
+                }
+            }
         }
+        System.out.println(out);
     }
 }
