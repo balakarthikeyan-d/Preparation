5967120a06ae6baa794bfb60405822b8ab693866
Balakarthikeyan <balakarthikeyan.d@zohocorp.com>
2023-12-04 12:00:45 +0530

    Practice update

--- Practice.java
+++ Practice.java
@@ -1,60 +1,32 @@
+import java.util.Set;
+import java.util.TreeSet;
+
 public class Practice {
     public static void main(String[] args) {
-        int[][] points={{1,1},{3,4},{-1,0}};
-        int out=0;
-        for(int i=0;i+1<points.length;i++){
-            int[] a=points[i];
-            int[] b=points[i+1];
-            while(true){
-                if(a[0]>b[0] && a[1]>b[1]){
-                    a[0]=a[0]-1;
-                    a[1]=a[1]-1;
-                    out++;
-                    continue;
-                }
-                if(a[0]<b[0] && a[1]<b[1]){
-                    a[0]=a[0]+1;
-                    a[1]=a[1]+1;
-                    out++;
-                    continue;
-                }
-                if(a[0]>b[0] && a[1]<b[1]){
-                    a[0]=a[0]-1;
-                    a[1]=a[1]+1;
-                    out++;
-                    continue;
-                }
-                if(a[0]<b[0] && a[1]>b[1]){
-                    a[0]=a[0]+1;
-                    a[1]=a[1]-1;
-                    out++;
-                    continue;
-                }
-                if(a[0]==b[0] && a[1]>b[1]){
-                    a[1]=a[1]-1;
-                    out++;
-                    continue;
-                }
-                if(a[0]>b[0] && a[1]==b[1]){
-                    a[0]=a[0]-1;
-                    out++;
-                    continue;
-                }
-                if(a[0]==b[0] && a[1]<b[1]){
-                    a[1]=a[1]+1;
-                    out++;
-                    continue;
-                }
-                if(a[0]<b[0] && a[1]==b[1]){
-                    a[0]=a[0]+1;
-                    out++;
-                    continue;
-                }
-                if(a[0]==b[0] && a[1]==b[1]){
-                    break;
+        String num="00012233342222";
+        Set<String> perfect=new TreeSet<>();
+        char ch='-';
+        String perfectNums="";
+        for(int i=0;i+1<num.length();i++){
+            ch=num.charAt(i);
+            if(perfectNums.equals("")){
+                perfectNums+=String.valueOf(ch);
+            }
+            if(ch==num.charAt(i+1)){
+                perfectNums+=String.valueOf(ch);
+                if(perfectNums.length()==3){
+                    perfect.add(perfectNums);
+                    ch='-';
+                    perfectNums="";
                 }
             }
+            else{
+                ch='-';
+                perfectNums="";
+            }
         }
-        System.out.println(out);
+        Object[] out= perfect.stream().toArray();
+        String result=out.length==0?"":out[out.length-1].toString();
+        System.out.println(result);
     }
 }
