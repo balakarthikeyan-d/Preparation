93528e63457672d770d62ba59eb3e51d87ad51a4
Balakarthikeyan <balakarthikeyan.d@zohocorp.com>
2023-12-09 08:36:07 +0530

    TreeNode Understanding

--- TreeNodeConstruct.java
+++ TreeNodeConstruct.java
@@ -1,16 +1,5 @@
-import com.sun.source.tree.Tree;
-
-import java.util.Set;
-import java.util.TreeSet;
-public class Practice {
+public class TreeNodeConstruct {
     public static void main(String[] args) {
-//        TreeNode leftTree1=new TreeNode(6);
-//        TreeNode leftTree2=new TreeNode(5,leftTree1,null);
-//        TreeNode leftTree3=new TreeNode(4,leftTree2,null);
-//        TreeNode leftTree4=new TreeNode(3,leftTree3,null);
-//        TreeNode leftTree=new TreeNode(2,leftTree4,null);
-//        TreeNode root=new TreeNode(1,leftTree,null);
-
         TreeNode leftTree1=new TreeNode(5);
         TreeNode rightTree1=new TreeNode(6);
         TreeNode root1=new TreeNode(4,leftTree1,rightTree1);
@@ -20,7 +9,6 @@ public class Practice {
         TreeNode root=new TreeNode(0,root2,root1);
         System.out.println(treeConstruct(root));
     }
-
     public static String treeConstruct(TreeNode node) {
         String left = "";
         String right = "";
