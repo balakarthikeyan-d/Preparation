7a30c54936e37e5ac0a500a327bcbb1339807977
Balakarthikeyan <balakarthikeyan.d@zohocorp.com>
2023-12-15 12:05:21 +0530

    Practice

--- Practice.java
+++ Practice.java
@@ -1,46 +1,92 @@
 import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Practice {
     public static void main(String[] args) {
 //        int[][] arr= {{0,0,1,0},{0,0,0,0},{0,0,0,0},{0,1,0,0}};
-        int[][] arr= {{0,1},{1,0},{0,0}};
+        int[][] arr= {{1,1,1}};
+//        int[][] arr= {{1,1},{1,1},{1,1}};
         System.out.println(Arrays.deepToString(cou(arr)));
     }
 
     /*
-    * 0 1
-    * 1 0
-    * 0 0
+    * 0 1 0
+    * 1 0 1
+    * 1 0 0
     * diff[0][0] = onesRow0 + onesCol0 - zerosRow0 - zerosCol0
     * */
 
     public static int[][] cou(int[][] grid){
         int out[][]=new int[grid.length][grid[0].length];
-        for(int i=0;i< grid.length;i++){
-            int zerosRow=checkZerosRows(grid[i]);
-            int onesRow=grid.length-zerosRow;
-            for(int j=0;j<grid[0].length;j++){
-                int zeros=zerosCol(grid,j);
-                int ones=grid[i].length-zeros;
-                out[i][j]=onesRow+ones-zerosRow-zeros;
+        int[] onesRArr=new int[grid.length];
+        int[] zerosRArr=new int[grid.length];
+        int[] onesCArr=new int[grid[0].length];
+        int[] zerosCArr=new int[grid[0].length];
+        int diagnol=0,row=0,col=0;
+        double percent=(double)grid.length/(double)grid[0].length*100;
+        if(percent>80 && percent<130) {
+            while (true) {
+                int[] temp = grid[diagnol];
+                int onesRow = checkOnesRows(temp);
+                int zerosRow = temp.length - onesRow;
+                if (row < grid.length) {
+                    onesRArr[row] = onesRow;
+                    zerosRArr[row] = zerosRow;
+                    row++;
+                    if (row < grid.length)
+                        diagnol++;
+                }
+                if (col < temp.length) {
+                    int onesCol = checkOnesCols(grid, col);
+                    int zerosCol = grid.length - onesCol;
+                    onesCArr[col] = onesCol;
+                    zerosCArr[col] = zerosCol;
+                    col++;
+                }
+                if (row == grid.length && col == temp.length)
+                    break;
+            }
+            for(int i=0;i< grid.length;i++){
+                for (int j=0;j< grid[i].length;j++){
+                    out[i][j]=onesRArr[i]+onesCArr[j]-zerosRArr[i]-zerosCArr[j];
+                }
+            }
+        }
+        else{
+            int[] oneRow=new int[grid.length];
+            int[] oneCol=new int[grid[0].length];
+            Arrays.fill(oneRow, 0);
+            Arrays.fill(oneCol, 0);
+            for(int i=0;i<grid.length;i++){
+                for(int j=0;j<grid[0].length;j++){
+                    oneRow[i] += grid[i][j];
+                    oneCol[j] += grid[i][j];
+                }
+            }
+            for(int i=0;i< grid.length;i++){
+                for(int j=0;j<grid[0].length;j++){
+                    out[i][j]=2 * oneRow[i] + 2 * oneCol[j] - grid.length - grid[0].length;
+                }
             }
         }
         return out;
     }
-    static int zerosCol(int mat[][],int i){
-        int col=0;
-        for(int j=0;j<mat.length;j++){
-            if(mat[j][i]==0){
-                col++;
+
+    static int checkOnesRows(int[] temp){
+        int add=0;
+        for(int i=0;i<temp.length;i++){
+            if(temp[i]==1){
+                add++;
             }
         }
-        return col;
+        return add;
     }
-    static int checkZerosRows(int[] mat){
+    static int checkOnesCols(int[][] mat,int i){
         int add=0;
-        for(int i=0;i<mat.length;i++){
-            if(mat[i]==0){
+        for(int j=0;j<mat.length;j++){
+            if(mat[j][i]==1){
                 add++;
             }
         }
